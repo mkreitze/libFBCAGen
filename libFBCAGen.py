3ef042fc55884f06784a06094de72f6859e6cc5e
mkreitze <mkreitze@uoguelph.ca>
2021-03-03 02:37:49 -0500

    added in next functions to write out

--- libFBCAGen.py
+++ libFBCAGen.py
@@ -24,6 +24,7 @@ def initCA(CAMap,length = FBCAConsts.CALength, width = FBCAConsts.CAWidth):
         CAMap.append(holder)
     return(CAMap)
 
+
 # Copying function #
 ### Input: A list full of CACells
 ### Output: Another list full cells with the same states as the input
@@ -39,5 +40,106 @@ def copyOver(CAMapInit,length = FBCAConsts.CALength, width = FBCAConsts.CAWidth)
         CAMap.append(holder)
     return(CAMap)
 
+### DID ABOVE
 
-            
+# FBCA Updater #
+### Input: An L_n (2d list of CACells), A score matrix (stored as a 1d list)
+### Output: L_(n+1) (2d list of CACells)
+# Notes:
+# The update is done in 3 steps:
+# # Assigns each cell its score
+# # Generates a copy of the L_g
+# # Updates the states of the copy by taking the highest score state of a cells neighbourhood
+# # The copy is then returned
+# # The default neighbourhood is Von Neumann and is defined by the neighbours function 
+def updateMap(CAMap,scoreMatrix):
+
+    for x in range(0,CALength):
+        for y in range(0,CAWidth):
+            #Need to get score from center square and its neighbours.
+            row = CAMap[x][y].state*numOfStates #the center colour determines the row of the score matrix used 
+            #new method to save a small amount of computation
+            col0=CAMap[(x+neighbours[0][0])%CALength][(y+neighbours[0][1])%CAWidth].state 
+            col1=CAMap[(x+neighbours[1][0])%CALength][(y+neighbours[1][1])%CAWidth].state 
+            col2=CAMap[(x+neighbours[2][0])%CALength][(y+neighbours[2][1])%CAWidth].state 
+            col3=CAMap[(x+neighbours[3][0])%CALength][(y+neighbours[3][1])%CAWidth].state
+            CAMap[x][y].score=scoreMatrix[row+col0]+scoreMatrix[row+col1]+scoreMatrix[row+col2]+scoreMatrix[row+col3]
+    #start by copying the map
+    CAMapCopy=copyOver(CAMap)
+    #for every cell, find the highest score among neighbours
+    for x in range(0,CALength):
+        for y in range(0,CAWidth):
+            #NOTE: We give priority to the center square on ties. Priority continues up with the last defined neighbour to have the worst priority
+            bigScore=0;bigScore=CAMap[x][y].score
+            #compares neighbours scores, reassigning bigScore and state if someone is bigger
+            for z in neighbours:
+                if(bigScore<CAMap[(x+z[0])%CALength][(y+z[1])%CAWidth].score):
+                    bigScore=CAMap[(x+z[0])%CALength][(y+z[1])%CAWidth].score
+                    CAMapCopy[x][y].state=CAMap[(x+z[0])%CALength][(y+z[1])%CAWidth].state
+    return(CAMapCopy)            
+
+# Generation of FBCA #
+##Input: Score matrix (as a one dimensional list), directory, L_0 and a quantifer
+##Output: L_g for the system, if visualized L will be generated in a folder named by the quantifer.
+# Notes:
+# Since F,g,L_0,n are all constant, they are not needed for the generate FBCA function.
+# The global variables useImages and finalImage dictate if visual records are kept for generated FBCAs
+# The visual records come as a gif and L_g (I am having difficulty making it just a bunch of single images)
+def generateFBCA(scoreMatrix,d,CAMapInit,quantifer):
+    gif=[];CAMap=[];CAMap=copyOver(CAMapInit)
+    if (useImages==1) or (finalImage==1):
+        d=f"{d}/{quantifer}/"
+    makeFolder(d)
+    for n in range(numOfGens):
+        if (useImages==1):
+            gif.append(genIm(CAMap,numOfGens,d,quantifer))
+        CAMap=updateMap(CAMap,scoreMatrix)
+    if (finalImage==1):
+        gif.append(genIm(CAMap,numOfGens,d,quantifer))
+        gif[0].save(f"{d}{quantifer}.gif",save_all=True,append_images=gif[1:],optimize=False,duration=100,loop=0)
+    return(CAMap)
+
+# Folder generation #
+### Input: Name of a folder we want to make
+### Output: True (1) or False (0) if folder is made 
+def makeFolder(folderName):
+    try: 
+        os.makedirs(folderName)
+        return(1)
+    except:
+        return(0)
+
+# Image generation method #
+### Input: L_n (2d list of CACell with states), n, folder name, 
+### Output: An image file
+# Notes:
+# I have no idea what an image file is
+def genIm(CAMap,n,directory=os.getcwd(),quantifer="giveName"):
+    im= Image.new('RGB', (n, n))
+    for x in range(n):
+        for y in range(n):
+            im.putpixel((x,y),colourConvert(CAMap[x][y]))
+    im.save(f"{directory}{quantifer} {str(n)}.png")
+    return(im)
+
+# State to Colour converter #
+### Input: state (as an int)
+### Output: RGB touple
+# Notes:
+# When visualize through images, each state is given a colour. All colours are arbitrary with one exception:
+# # Some evolutionary computation attempts to force certain behaviours by constraning the first state 
+# # This was done to preserve 'open space' and as such the first state is always white.
+def colourConvert(x):
+    return {
+        0: (255,255,255),
+        1: (0,0,0),
+        2: (0,255,0),
+        3: (0,0,255),
+        4: (255,0,0),
+        5: (51,255,255),
+        6: (0,255,255),
+        7: (255,69,0),
+        8: (0,102,0),
+        9: (153,0,153),
+        10: (255,255,51),
+    }[x]    
